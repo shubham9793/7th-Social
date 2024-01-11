83f310c2c0d99d30b27f58ad8779e634790e5ea1
SHUBHAM SAHU <shubhamsahu9793@gmail.com>
2024-01-11 16:12:16 +0530

    Remove todo-app-version-3-final directory

--- todo-app-version-3-final/src/components/WelcomeMessage.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./WelcomeMessage.module.css";
-const WelcomeMessage = () => {
-  return <p className={styles.welcomeMessage}>Enjoy your day!.</p>;
-};
-
-export default WelcomeMessage;
