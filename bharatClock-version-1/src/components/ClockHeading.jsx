2afa33ad46e5abfab018df93a8a9e0841d1bc843
SHUBHAM SAHU <shubhamsahu9793@gmail.com>
2024-01-11 16:06:36 +0530

    Remove bharatClock-version-1 directory

--- bharatClock-version-1/src/components/ClockHeading.jsx
+++
@@ -1,7 +0,0 @@
-function ClockHeading() {
-  let headingName = "Bharat Clock";
-
-  return <h1 className="fw-bolder">{headingName}</h1>;
-}
-
-export default ClockHeading;
--- bharatClock-version-1/src/components/ClockSlogan.jsx
+++
@@ -1,8 +0,0 @@
-function ClockSlogan() {
-  return (
-    <p className="lead">
-      This is the clock that shows the time in Bhart at all times
-    </p>
-  );
-}
-export default ClockSlogan;
